21aaf2c01ed110b5aa7b491d5a5abffaa10a0061
Nuhang Subba <nuhang@Nuhangs-MacBook-Pro.local>
2019-04-02 20:57:16 +0100

    Changed the function

--- src/padleft.cpp
+++ src/padleft.cpp
@@ -1,16 +1,22 @@
 
-#include <padleft.hpp>
+#include <iostream>
 
-namespace pl {
 
 
 std::string pad_left(const std::string& str, int num_spaces)
 {
     std::string out = str;
+    std::string out1 = "";
     for (int i = 0; i < num_spaces; i++) {
-        out = " " + out; // this is rubbish ;)
+        out1 = " " + out1; // this is rubbish ;)
     }
+    out = out1 + out;
     return out;
+   
 }
 
+
+int main(){
+    const std::string str = "Hello";
+    std::cout << pad_left(str, 3)<< std::endl;
 }
